c9b408b97183bdad61b88a86ab89a46d8cdfdf5c
Ponragavan M <141037337+Ponragavan@users.noreply.github.com>
2024-05-18 14:19:48 +0530

    Delete Custom-Hooks directory

--- Custom-Hooks/src/components/ForwardCounter.js
+++
@@ -1,9 +0,0 @@
-import useCounter from '../hooks/useCounter';
-import Card from './Card';
-
-const ForwardCounter = () => {
-  const counter = useCounter();
-  return <Card>Forward : {counter}</Card>;
-};
-
-export default ForwardCounter;
